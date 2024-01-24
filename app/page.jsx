86d14378a836485abbf1fc6fd6cb6807baf0df06
Aman Relan <aman.relan-ext@jci.com>
2024-01-25 02:03:01 +0530

    Added different components

--- app/page.jsx
+++ app/page.jsx
@@ -4,20 +4,27 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center h-screen bg-black text-white'>
-      <div className='max-w-7xl'>
-        <Slider>
-          {images.map((picture, index) => (
-            <Image
-              key={index}
-              src={picture.src}
-              alt={picture.title}
-              width={1500}
-              height={2000}
-            />
-          ))}
-        </Slider>
-      </div>
-    </main>
+    <div>
+      <section className='py-24 bg-white text-black'>
+        <div className='container'>
+          <h1 className='text-3xl font-bold'>First NextJS Application</h1>
+        </div>
+      </section>
+      <main className='flex justify-center items-center h-screen bg-black text-white'>
+        <div className='max-w-7xl'>
+          <Slider>
+            {images.map((picture, index) => (
+              <Image
+                key={index}
+                src={picture.src}
+                alt={picture.title}
+                width={1500}
+                height={2000}
+              />
+            ))}
+          </Slider>
+        </div>
+      </main>
+    </div>
   )
 }
